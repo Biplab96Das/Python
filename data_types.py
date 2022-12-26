59d1300dc61bda3d40d3f981ef15404beab07fa4
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-26 19:34:51 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,10 @@
+#String
+print("Hello"[1])
+print("Hello"[0])
+print("Hello"[4])
+print("123"+"345")
+#Interger
+print(123+345)
+print(123_456_789)#123,456,789 is written in python using 123_456_789
+#Floating Point Number : Decimal point float around the numbers
+print(1.34568)
