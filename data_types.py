1c4cb3dc4f4838de4a0c850c9e06bbffa0b0a664
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-12-26 19:40:30 +0530

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -8,3 +8,6 @@ print(123+345)
 print(123_456_789)#123,456,789 is written in python using 123_456_789
 #Floating Point Number : Decimal point float around the numbers
 print(1.34568)
+#Boolean
+print(True)
+print(False)
